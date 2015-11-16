64aa81c1c12b870da6de6a9979a88e67a7184287
CatLovesPurple <1208bananalas@gmail.com>
2015-11-16 07:11:51 -0800

    Add left/right visible

--- controller.js
+++ controller.js
@@ -5,20 +5,16 @@ app.controller("sidebar", function ($scope, $rootScope) {
     $scope.close = function () {
         $scope.leftVisible = false;
         $scope.rightVisible = false;
-        $scope.visible = false;
-
     };
 
     $scope.showLeft = function (e) {
         $scope.leftVisible = true;
         e.stopPropagation();
-        $scope.visible = true;
     };
 
     $scope.showRight = function (e) {
         $scope.rightVisible = true;
         e.stopPropagation();
-        $scope.visible = true;
     }
 
     $rootScope.$on("documentClicked", _close);
